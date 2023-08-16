3e491079578fe36b23158ea3e56520ff7bc3f4a5
beatmejy <beatmejy@naver.com>
2023-08-16 12:49:34 +0900

    refactor: 주소 테이블 변경. 시군구, 동만 저장.

--- auth/src/main/java/project/swithme/auth/domain/district/entity/District.java
+++ auth/src/main/java/project/swithme/auth/domain/district/entity/District.java
@@ -14,142 +14,18 @@ public class District extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "building_name", length = 70)
-    private String buildingName;
-
-    @Column(name = "building_no1", length = 5)
-    private String buildingNo1;
-
-    @Column(name = "building_no2", length = 4)
-    private String buildingNo2;
-
-    @Column(name = "building_num", length = 25)
-    private String buildingNum;
-
     @Column(name = "dong", length = 20)
     private String dong;
-
-    @Column(name = "dong_code", length = 10)
-    private String dongCode;
-
-    @Column(name = "dong_hj", length = 30)
-    private String dongHj;
-
-    @Column(name = "doro", length = 40)
-    private String doro;
-
-    @Column(name = "doro_code", length = 12)
-    private String doroCode;
-
-    @Column(name = "doro_en", length = 50)
-    private String doroEn;
-
-    @Column(name = "eupmyun", length = 20)
-    private String eupmyun;
-
-    @Column(name = "eupmyun_en", length = 25)
-    private String eupmyunEn;
-
-    @Column(name = "eupmyundong_no", length = 2)
-    private String eupmyundongNo;
-
-    @Column(name = "jibun1", length = 4)
-    private String jibun1;
-
-    @Column(name = "jibun2", length = 4)
-    private String jibun2;
-
-    @Column(name = "mount_yn", length = 1)
-    private String mountYn;
-
-    @Column(name = "multiple", length = 1)
-    private String multiple;
-
-    @Column(name = "ri", length = 20)
-    private String ri;
-
-    @Column(name = "sido", length = 25)
-    private String sido;
-
-    @Column(name = "sido_en", length = 20)
-    private String sidoEn;
 
     @Column(name = "sigungu", length = 30)
     private String sigungu;
 
-    @Column(name = "sigungu_en", length = 30)
-    private String sigunguEn;
-
-    @Column(name = "under_yn", length = 1)
-    private String underYn;
-
-    @Column(name = "zipcode", length = 5)
-    private String zipcode;
-
-    @Column(name = "zipcode_old", length = 7)
-    private String zipcodeOld;
-
-    @Column(name = "zipcode_seq", length = 3)
-    private String zipcodeSeq;
-
     protected District() {
     }
 
-    public District(Long id,
-        String buildingName,
-        String buildingNo1,
-        String buildingNo2,
-        String buildingNum,
-        String dong,
-        String dongCode,
-        String dongHj,
-        String doro,
-        String doroCode,
-        String doroEn,
-        String eupmyun,
-        String eupmyunEn,
-        String eupmyundongNo,
-        String jibun1,
-        String jibun2,
-        String mountYn,
-        String multiple,
-        String ri,
-        String sido,
-        String sidoEn,
-        String sigungu,
-        String sigunguEn,
-        String underYn,
-        String zipcode,
-        String zipcodeOld,
-        String zipcodeSeq
-    ) {
-        this.id = id;
-        this.buildingName = buildingName;
-        this.buildingNo1 = buildingNo1;
-        this.buildingNo2 = buildingNo2;
-        this.buildingNum = buildingNum;
+    public District(String dong, String sigungu) {
         this.dong = dong;
-        this.dongCode = dongCode;
-        this.dongHj = dongHj;
-        this.doro = doro;
-        this.doroCode = doroCode;
-        this.doroEn = doroEn;
-        this.eupmyun = eupmyun;
-        this.eupmyunEn = eupmyunEn;
-        this.eupmyundongNo = eupmyundongNo;
-        this.jibun1 = jibun1;
-        this.jibun2 = jibun2;
-        this.mountYn = mountYn;
-        this.multiple = multiple;
-        this.ri = ri;
-        this.sido = sido;
-        this.sidoEn = sidoEn;
         this.sigungu = sigungu;
-        this.sigunguEn = sigunguEn;
-        this.underYn = underYn;
-        this.zipcode = zipcode;
-        this.zipcodeOld = zipcodeOld;
-        this.zipcodeSeq = zipcodeSeq;
     }
 
     @Override
@@ -160,11 +36,11 @@ public class District extends BaseEntity {
         if (!(object instanceof District district)) {
             return false;
         }
-        return getId().equals(district.getId());
+        return getDong().equals(district.getDong()) && getSigungu().equals(district.getSigungu());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(dong, sigungu);
     }
 }
