acf4ab75baf3ad73b3f9ca8e36490ed03e9951e4
beatmejy <beatmejy@naver.com>
2023-08-16 12:58:40 +0900

    feat: 시군구, 동만 전국 엑셀데이터로 저장.

---
+++ auth/src/main/java/project/swithme/auth/domain/district/service/DistrictService.java
@@ -0,0 +1,19 @@
+package project.swithme.auth.domain.district.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.swithme.auth.domain.district.entity.District;
+import project.swithme.auth.domain.district.repository.DistrictJpaRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DistrictService {
+
+    private final DistrictJpaRepository districtJpaRepository;
+
+    public List<District> saveAll(List<District> districtList) {
+        return districtJpaRepository.saveAll(districtList);
+    }
+}
