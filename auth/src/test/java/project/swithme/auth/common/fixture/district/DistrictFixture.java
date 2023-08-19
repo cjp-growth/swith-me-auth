948b150af270b22587f60845f65e728641ba33ce
beatmejy <beatmejy@naver.com>
2023-08-20 02:03:06 +0900

    test: DistrictFixture private 생성자 추가하기.

--- auth/src/test/java/project/swithme/auth/common/fixture/district/DistrictFixture.java
+++ auth/src/test/java/project/swithme/auth/common/fixture/district/DistrictFixture.java
@@ -4,6 +4,10 @@ import project.swithme.auth.domain.district.entity.District;
 
 public final class DistrictFixture {
 
+    private DistrictFixture() {
+        throw new AssertionError("올바른 방식으로 생성자를 호출해주세요.");
+    }
+
     public static final District sadangDistrict = new District("사당동", "서울특별시");
     public static final District sillimDistrict = new District("신림동", "서울특별시");
 
